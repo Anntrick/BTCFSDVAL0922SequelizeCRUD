c7c80e8a880488093c1afb0f646c331ea6bb8d63
Anntrick <anntrick91@gmail.com>
2022-10-25 13:47:05 +0200

    añadidas rutas

--- index.js
+++ index.js
@@ -3,107 +3,17 @@ const express = require('express')
 const app = express()
 
 const db = require('./db/db')
-const Usuario = require('./models/usuario')
+const Perfil = require('./models/perfil')
+
+const router = require('./router')
+
 
 const PORT = 3000
 
 //middleware
 app.use(express.json())
 
-
-// CRUD READ
-app.get('/', (req, res) => {
-    Usuario.findAll({
-        where: { sexo: 'Mujer' },
-        limit: 1
-    })
-    .then(resp => {
-        res.send(resp)
-    })
-})
-
-//Buscamos por Primary key
-app.get('/findID/:mail', (req, res) => {
-    let mail = req.params.mail
-    Usuario.findByPk(mail)
-    .then(resp => {
-        res.send(resp)
-    })
-})
-
-
-// CRUD CREATE
-app.post('/register', async (req, res) => {
-    try {
-        let data = req.body
-        let resp = await Usuario.create({
-            mail: data.mail,
-            foto: data.foto,
-            sexo: data.sexo,
-            fecha_nacimiento: data.fecha_nacimiento,
-            telefono: data.telefono,
-            contrasena: data.contrasena
-        })
-
-        res.send(resp)
-    } catch(error) {
-        res.send(error)
-    }
-})
-
-// CRUD UPDATE
-
-app.put('/updateUser', async (req, res) => {
-    try{
-        let data = req.body
-
-        let resp = await Usuario.update(
-            {
-                foto: data.foto,
-                sexo: data.sexo,
-                fecha_nacimiento: data.fecha_nacimiento,
-                telefono: data.telefono,
-                contrasena: data.contrasena
-            }, 
-            {
-                where: { mail: data.mail}
-            })
-
-        res.send({
-            resp: resp,
-            message: "Se ha actualizado el registro correctamente",
-            id: data.mail
-        })
-
-    } catch(error){
-        res.send(error)
-    }
-})
-
-// CRUD DELETE
-
-app.delete('/deleteUser/:mail', async (req, res) => {
-    try{
-        
-        let mail = req.params.mail
-        let resp = await Usuario.destroy({
-            where: { mail: mail }
-        })
-        console.log(resp)
-        if(resp == 1){
-            res.send("El usuario ha sido eliminado")
-        } else {
-            res.send("No se ha podido eliminar el usuario")
-        }
-        
-    } catch(error){
-        res.send(error)
-    }
-})
-
-
-
-
+app.use(router)
 
 
 
---
+++ views/ProfileRoutes.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const router = express.Router()
+
+const Perfil = require('../models/perfil')
+
+// CRUD READ PERFIL
+router.get('/getPerfil', (req, res) => {
+    Perfil.findAll()
+    .then(resp => {
+        res.send(resp)
+    })
+})
+
+// CRUD CREATE PERFIL
+router.post('/newProfile', async (req, res) => {
+    try {
+        let data = req.body
+        let resp = await Perfil.create({
+            usuario: data.usuario,
+            nombre: data.nombre,
+            seguidos: data.seguidos,
+            seguidores: data.seguidores,
+            biografia: data.biografia,
+            mail_user: data.mail_user
+        })
+
+        res.send(resp)
+
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+// CRUD UPDATE PERFIL
+router.put('/updateProfile', async (req, res) => {
+    try {
+        let data = req.body
+        let resp = await Perfil.update(
+            {
+                nombre: data.nombre,
+                seguidos: data.seguidos,
+                seguidores: data.seguidores,
+                biografia: data.biografia
+            },
+            {
+                where: { usuario: data.usuario }
+            }
+        )
+        
+        res.send(resp)
+
+    } catch(err) {
+        res.send(err)
+    }
+})
+
+// CRUD DELETE PERFIL
+router.delete('/deleteProfile/:user', async (req, res) => {
+    try{
+        let user = req.params.user
+        let resp = await Perfil.destroy({
+            where: { usuario: user }
+        })
+
+        if(resp == 1) {
+            res.send("Se ha eliminado el perfil correctamente")
+        } else {
+            res.send("No se ha podido eliminar el registro")
+        }
+
+    } catch(err) {
+
+    }
+})
+
+
+module.exports = router
